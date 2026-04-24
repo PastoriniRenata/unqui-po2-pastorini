581e728d3583e811fe4673e38d7d22386ad53ee2
Renata Pastorini <renata.pastorini.unq@gmail.com>
2026-04-23 23:34:26 -0300

    Estructura inicial de tps

---
+++ tp2/test/tp2/testTp2.java
@@ -0,0 +1,14 @@
+package tp2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testTp2 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
